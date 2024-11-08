000694e97c8467ca892f92a0e10928091562ead0
anderbytes <anderbytes@users.noreply.github.com>
2024-11-08 16:24:46 -0300

    Name fix for main Library

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='Organizze-Wrapper',
-    version='1.1.2',
+    version='1.2.0',
     packages=find_packages(),
     install_requires=[
         "requests>=2.32.3",
-        "PyMultiHelper>=1.1.4"
+        "PyMultiHelper>=1.1.11"
     ],
     description='Biblioteca Python de Wrapper para a API do Organizze.com.br',
     author='Anderson',
